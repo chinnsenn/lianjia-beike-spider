822c86cb56a271a5313df8f61a290f1520e692ff
only <iamonlyleo@gmail.com>
2019-07-23 15:57:05 +0800

    新增手动输入起始页功能

--- lib/spider/ningbo_spider.py
+++ lib/spider/ningbo_spider.py
@@ -16,25 +16,41 @@ from urllib.parse import urlparse
 from urllib.parse import parse_qs
 import lib.utility.version
 
+# w：以写方式打开， 
+# a：以追加模式打开 (从 EOF 开始, 必要时创建新文件) 
+# r+：以读写模式打开 
+# w+：以读写模式打开 (参见 w ) 
+# a+：以读写模式打开 (参见 a ) 
+# rb：以二进制读模式打开 
+# wb：以二进制写模式打开 (参见 w ) 
+# ab：以二进制追加模式打开 (参见 a ) 
+# rb+：以二进制读写模式打开 (参见 r+ ) 
+# wb+：以二进制读写模式打开 (参见 w+ ) 
+# ab+：以二进制读写模式打开 (参见 a+ )
 
 class NingboSpider(base_spider.BaseSpider):
     def collect_ningbo_record_data(self, fmt="csv"):
         csv_file = self.today_path + "/{0}.csv".format("ningbo")
-        with open(csv_file, "w", newline='', encoding='utf-8-sig') as f:
-            ningbos = self.get_ningbo_record_info()
+        if self.page_start == '1':
+            open_mode = "w"
+        else:
+            open_mode = "a+"
+        with open(csv_file, open_mode, newline='', encoding='utf-8-sig') as f:
+            ningbos = self.get_ningbo_record_info(self.page_start)
             if self.mutex.acquire(1):
                 self.total_num += len(ningbos)
                 self.mutex.release()
             if fmt == "csv":
                 for ningbo in ningbos:
-                    f.write(self.date_string + "," + ningbo.text() + "\n")
+                    f.write(ningbo.text() + "\n")
             print("Finish crawl ,save data to : " + csv_file)
 
     @staticmethod
-    def get_ningbo_record_info():
+    def get_ningbo_record_info(page_start = '1'):
         total_page = 1
         ningbo_list = list()
-        ningbo_list.append(Ningbo("合同签订日期","合同编号","所在区","街道（小区）","经纪机构备案名称"))
+        if page_start == '1':
+            ningbo_list.append(Ningbo("合同签订日期","合同编号","所在区","街道（小区）","经纪机构备案名称"))
         page = 'https://esf.cnnbfdc.com/contract'
         print(page)
         headers = create_headers()
@@ -49,12 +65,12 @@ class NingboSpider(base_spider.BaseSpider):
             parsed_result = urlparse(href)
             querys = parse_qs(parsed_result.query)
             querys = {k: v[0] for k, v in querys.items()}
-            total_page = int(querys['page'])
-            print("total = " + querys['page'])
+            total_page = querys['page']
+            print("total = " + total_page)
         except Exception as e:
             print(e)
 
-        for page_num in range(1, total_page + 1):
+        for page_num in range(int(page_start), int(total_page) + 1):
             page = 'https://esf.cnnbfdc.com/contract?page={0}'.format(page_num)
             print(page)
             headers = create_headers()
@@ -77,9 +93,9 @@ class NingboSpider(base_spider.BaseSpider):
                     print(ningbo.text() + '\n')
                     ningbo_list.append(ningbo)
         return ningbo_list
-    def start(self):
+    def start(self,page_start = '1'):
         self.today_path = create_date_path("ningbo_housing_estates", "ningbo", self.date_string)
-
+        self.page_start = page_start
         t1 = time.time()
         self.collect_ningbo_record_data()
         # 计时结束，统计结果
--- ningbo.py
+++ ningbo.py
@@ -4,8 +4,23 @@
 # 此代码仅供学习与交流，请勿用于商业用途。
 # 获得指定城市的所有新房楼盘数据
 
+import sys
+from lib.utility.version import PYTHON_3
 from lib.spider.ningbo_spider import *
 
 if __name__ == "__main__":
-    spider = NingboSpider("")
-    spider.start()
+    page_start = "请输入起始页码,直接回车从第一页开始爬取\n(注:如果从第一页开始爬取会覆盖同一天目录下同名文件的数据，注意备份):\n"
+    if not PYTHON_3:  # 如果小于Python3
+        page = raw_input(page_start)
+    else:
+        page = input(page_start)
+    if page is None or len(page) == 0:
+        print("从第1页开始爬去数据")
+        spider = NingboSpider(base_spider.NINGBO_SPIDER)
+        spider.start()
+    elif int(page) < 1:
+        print("请输入有效数字")
+    else:
+        print("从第" + page + "页开始爬取数据")
+        spider = NingboSpider(base_spider.NINGBO_SPIDER)
+        spider.start(page)
