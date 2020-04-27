839564240552259c4281d42c8295d08a8dfedc5d
chinnsenn <iamonlyleo@gmail.com>
2020-04-27 22:51:51 +0800

    完善 rd.py 去重功能

--- rd.py
+++ rd.py
@@ -5,17 +5,12 @@ import pandas as pd
 import os
 
 
-def filereadcsv(originFile_path, toFile_path):
+def filereadcsv(originFile_path, toFile_path,duplicate_key):
+    if  os.path.exists(toFile_path):
+        os.remove(toFile_path)
     df = pd.read_csv(originFile_path,delimiter=',')
     df.drop_duplicates('核验编码').to_csv(toFile_path)
-    # for chunks in pd.read_csv(originFile_path, chunksize=50000, header=0, delimiter=","):
-    #     try:
-    #         print(chunks.info())
-    #         chunks.drop_duplicates('核验编码').to_csv(toFile_path, mode='a', header=False)
-    #     except Exception as e:
-    #         print(e)
-    #         continue
-
+    print("去重成功:" + toFile_path)
 
 if __name__ == "__main__":
     prompt_start = "请拖入需要去重的 csv 文件:\n"
@@ -23,4 +18,9 @@ if __name__ == "__main__":
     if not os.path.exists(csv_file):
         print("请拖入有效文件")
     else:
-        filereadcsv(csv_file, os.path.splitext(csv_file)[0] + "_rd.csv")
+        df = pd.read_csv(csv_file,chunksize=1)
+        for index,s in enumerate(df._engine.orig_names):
+            print("{0},{1}".format(index,s),end="，")
+        prompt_duplicate = "\n请选择根据哪列去重:\n"
+        index_key = input(prompt_duplicate)
+        filereadcsv(csv_file, os.path.splitext(csv_file)[0] + "_去重.csv",df._engine.orig_names[int(index_key)])
